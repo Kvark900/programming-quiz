060f07e375961e92efac3cdb788718223fd2c7fa
Kvark900 <kvant800@gmail.com>
2020-05-10 14:38:41 +0200

    share option added

--- app/src/main/java/ba/unsa/pmf/result/GameResultFragment.kt
+++ app/src/main/java/ba/unsa/pmf/result/GameResultFragment.kt
@@ -16,22 +16,24 @@
 
 package ba.unsa.pmf.result
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import ba.unsa.pmf.R
 import ba.unsa.pmf.databinding.FragmentGameResultBinding
 import ba.unsa.pmf.game.GameViewModel
 import ba.unsa.pmf.settings.GameSettingsViewModel
+import kotlin.math.log
 
 class GameResultFragment : Fragment() {
     private val gameViewModel: GameViewModel by activityViewModels()
     private val gameSettingsViewModel: GameSettingsViewModel by activityViewModels()
+    private lateinit var resultMessageText: String
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -44,14 +46,34 @@ class GameResultFragment : Fragment() {
         val numberOfQuestions = gameSettingsViewModel.settings.numberOfQuestions.number
         val numOfCorrectAnswers = gameViewModel.numOfCorrectAnswers
         val percent = numOfCorrectAnswers.toDouble() / numberOfQuestions * 100
+        resultMessageText = "${numOfCorrectAnswers}/${numberOfQuestions} correct answers"
 
-        binding.resultMessage.text = "${numOfCorrectAnswers}/${numberOfQuestions} correct answers"
+        binding.resultMessage.text = resultMessageText
         binding.percentageText.text = "${percent}%"
         binding.nextMatchButton.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_gameResultFragment_to_gameSettingsFragment)
             gameViewModel.numOfCorrectAnswers = 0
             gameViewModel.questionIndex = 0
         }
+        setHasOptionsMenu(true)
         return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.winner_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, resultMessageText)
+            type = "text/plain"
+        }
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
+        return true
+    }
+
+
 }
