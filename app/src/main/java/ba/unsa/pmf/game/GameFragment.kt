075bedf90c188c1166482de5692c9e8c7f027b15
Kvark900 <kvant800@gmail.com>
2020-05-15 18:17:11 +0200

    refactoring

--- app/src/main/java/ba/unsa/pmf/game/GameFragment.kt
+++ app/src/main/java/ba/unsa/pmf/game/GameFragment.kt
@@ -151,7 +151,7 @@ class GameFragment : Fragment() {
         item.isEnabled = true
     }
 
-    fun View.hideKeyboard() {
+    private fun View.hideKeyboard() {
         val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(windowToken, 0)
     }
